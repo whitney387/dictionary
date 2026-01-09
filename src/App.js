433b40f0038799f1c2d2bedcab4cdac5751cc10f
Whitney-Murugi <murugiwhitney01@gmail.com>
2026-01-09 18:16:26 +0300

    added header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <h1 class="header text-center">Dictionary</h1>
+        < h2 className="header-2">What word do you want to look up</h2>
         <DictionarySearch></DictionarySearch>
         <footer className=" app-footer">
           <small>This project was coded by Whitney Mmaitsi</small>
