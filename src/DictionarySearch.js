778996bae747579eaeb5024def7637a8df14b554
Whitney-Murugi <murugiwhitney01@gmail.com>
2026-01-09 14:47:10 +0300

    Added results to the page

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,33 +1,41 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./DictionarySearch.css";
+import Results from "./Resuts";
 
 export default function DictionarySearch() {
   let [keyWord, setKeyWord] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data);
   }
 
   function search(event) {
     event.preventDefault();
-    console.log(`searching for ${keyWord}`);
-  }
 
-  function handleKeywordChange(event) {
-    setKeyWord(event.target.value);
-
+    // API Call should happen here when you press ENTER
     let apiKey = "23e2742eef1f1bc7tod430e337aaf4bd";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`;
 
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleKeywordChange(event) {
+    setKeyWord(event.target.value);
+  }
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+        <input
+          type="search"
+          autoFocus={true}
+          onChange={handleKeywordChange}
+          placeholder="Search for a word..."
+        />
       </form>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Resuts.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Results(props) {
+  if (props.results) {
+    return (
+      <div className="Results">
+        <section>
+          <h2>{props.results.word}</h2>
+          <p className="phonetic">{props.results.phonetic}</p>
+        </section>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <section key={index}>
+              <h3>{meaning.partOfSpeech}</h3>
+              <p>
+                <strong>Definition: </strong>
+                {meaning.definition}
+                <br />
+                <em>{meaning.example}</em>
+              </p>
+            </section>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
