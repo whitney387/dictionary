9e0ccab2da8bf61849526841b21427cc2f390afc
Whitney-Murugi <murugiwhitney01@gmail.com>
2026-01-09 10:50:12 +0300

    Added the dictionary api

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./DictionarySearch.css";
 
 export default function DictionarySearch() {
   let [keyWord, setKeyWord] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyWord}`);
+    console.log(`searching for ${keyWord}`);
   }
 
   function handleKeywordChange(event) {
     setKeyWord(event.target.value);
+
+    let apiKey = "23e2742eef1f1bc7tod430e337aaf4bd";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
